4d3ad298bb6f3049cab0e292cfa86bcb095d3a5e
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-04-30 02:59:01 -0300

    style: Adicionando comentarios.

--- src/NovatecScraper.py
+++ src/NovatecScraper.py
@@ -6,6 +6,7 @@ from src.Book import Book
 
 
 class NovatecScraper:
+    # Criando atributos para url e nossas regex.
     URL = "https://novatec.com.br/lista.php?id=3"
     REGEX_BOOK = re.compile(r"livros/")
     REGEX_AUTHOR = re.compile(r"autores/")
@@ -13,19 +14,24 @@ class NovatecScraper:
     REGEX_PAGES = re.compile(r"Páginas:\s*(\d+)")
     REGEX_PRICE = re.compile(r"Preço:\s*R\$\s*(\d+,\d{2})")
 
+    # Criando atributos soup e books.
     def __init__(self):
         self.soup = None
         self.books = []
 
+    # Atribuindo valor ao atributo soup.
     def fetch(self):
         html = urlopen(self.URL)
         self.soup = BeautifulSoup(html, "html.parser")
 
+    # Caso ocorra um erro ao atribuir valor ao soup.
     def parse(self):
         if self.soup is None:
             raise RuntimeError("Fetch must be called before parse.")
 
+    # Pegando todos os livros da pagina.
     def get_books(self):
+        # Criando variaveis que iremos utilizar para armazenar os dados
         all_fonts = self.soup.find_all("font")
         all_links_fonts = []
         font_from_link = []
@@ -34,6 +40,7 @@ class NovatecScraper:
         all_years = []
         all_pages = []
         all_prices = []
+
         # Pegando todas as fonts, pois nelas estão os dados que precisamos
         for font in all_fonts:
             # Se nossa font tiver um ancora com o href="...livros/..."
@@ -53,6 +60,7 @@ class NovatecScraper:
             all_pages.append(re.findall(self.REGEX_PAGES, font.text)[0])
             all_prices.append(re.findall(self.REGEX_PRICE, font.text)[0])
 
+        # Criamos os livros utilizando classe Book.
         for i in range(0, 10):
             self.books.append(Book(all_titles[i], all_authors[i], all_years[i], all_pages[i], all_prices[i]))
 
