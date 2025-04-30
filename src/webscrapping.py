9445ed8dc73df59fb846e54209ffd4b5525babc7
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-04-30 01:45:58 -0300

    feat: Encontrando os titulos

--- src/webscrapping.py
+++ src/webscrapping.py
@@ -2,19 +2,23 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
 
-html = urlopen("https://novatec.com.br/")
+
+html = urlopen("https://novatec.com.br/lista.php?id=3")
 soup = BeautifulSoup(html, "html.parser")
 
-regex_livros = re.compile(r"/livros")
+regex_livros = re.compile(r"livros/")
 regex_qualquercoisa = re.compile(r".*")
 
-all_links = soup.find_all("a", {"href":regex_livros})
+all_fonts = soup.find_all("font")
 
-print(all_links)
-
+all_links_fonts = []
+font_from_link = []
 all_titles = []
 
-for link in all_links:
-    if link.find("img", {"alt": regex_qualquercoisa}):
-        all_titles.append(link.find("img", {"alt": regex_qualquercoisa}).get)
+for font in all_fonts:
+    if font.find_all("a", {"href": regex_livros}):
+        all_links_fonts.append(font.find_all("a", {"href": regex_livros}))
+        font_from_link.append(font)
+        all_titles.append(all_links_fonts[-1][0].text)
 
+print(all_titles)
